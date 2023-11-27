17ac5ae60395ebc8d36d4fc19baa6cb9eacbc2aa
Cameron King <35708477+cameronking4@users.noreply.github.com>
2023-11-27 01:09:47 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
   async redirects() {
     return [
       {
         source: '/github',
-        destination: 'https://github.com/Nutlope/twitterbio',
+        destination: 'https://github.com/cameronking4/sketch2code'
         permanent: false,
       },
       {
         source: '/deploy',
-        destination: 'https://vercel.com/templates/next.js/twitter-bio',
+        destination: 'https://vercel.com/templates',
         permanent: false,
       },
     ];

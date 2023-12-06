bc98efc0a458f127a3b632a7f7155ab49656dc47
Cameron King <35708477+cameronking4@users.noreply.github.com>
2023-12-06 00:07:31 -0800

    Sandpack

--- components/Sandpack.jsx
+++ components/Sandpack.jsx
@@ -4,6 +4,25 @@ export default function Sandbox({ mainFile, framework }) {
 
       const reactFiles = {
         "/App.js": mainFile,
+        "/README.md": {
+          code: `# [Sketch-2-App](https://www.sketch2app.io/)
+
+          ## Use GPT4v to generate web app code (CRA + Tailwind)
+          Use React to generate a web app with Tailwind CSS. It will generate code and a sandbox to preview the app within seconds of capturing your wireframe/sketch
+          
+          [![Sketch2Code](https://markdown-videos-api.jorgenkh.no/url?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D1VC_a0JP7TM)](https://www.youtube.com/watch?v=1VC_a0JP7TM)
+        
+          ## Running Locally
+          
+          After cloning the repo, go to [OpenAI](https://beta.openai.com/account/api-keys) to make an account and put your API key in a file called \`.env\`.
+          
+          Then, run the application in the command line and it will be available at \`http://localhost:3000\`.
+          
+          \`\`\`bash
+          npm install && npm start
+          \`\`\`
+        `          
+        },
         "/index.js": {
           code: `import React from "react";
       import ReactDOM from "react-dom";
@@ -109,6 +128,25 @@ export default function Sandbox({ mainFile, framework }) {
 
       const rnFiles = {
         "/App.js": mainFile,
+        "/README.md": {
+          code: `# [Sketch-2-App](https://www.sketch2app.io/)
+
+          ## Use GPT4v to generate web app code (CRA + Tailwind)
+          Use React Native to generate a web mobile app. It will generate code and a sandbox to preview the app within seconds of capturing your wireframe/sketch
+          
+          [![Sketch2Code](https://markdown-videos-api.jorgenkh.no/url?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D1VC_a0JP7TM)](https://www.youtube.com/watch?v=1VC_a0JP7TM)
+        
+          ## Running Locally
+          
+          After cloning the repo, go to [OpenAI](https://beta.openai.com/account/api-keys) to make an account and put your API key in a file called \`.env\`.
+          
+          Then, run the application in the command line and it will be available at \`http://localhost:3000\`.
+          
+          \`\`\`bash
+          npm install && npm start
+          \`\`\`
+        `          
+        },
         "/package.json": {
           code: `{
             "name": "react-native-expo",

c53b4f4bbbbb2b8842f13f0b5205c5aa237f0882
Cameron King <35708477+cameronking4@users.noreply.github.com>
2023-12-06 00:39:41 -0800

    Sandpack

--- components/Sandpack.jsx
+++ components/Sandpack.jsx
@@ -6,34 +6,28 @@ export default function Sandbox({ mainFile, framework }) {
         "/App.js": mainFile,
         "/README.md": {
           code: `# [Sketch-2-App](https://www.sketch2app.io/)
+## Use GPT4v to generate web app code (CRA + Tailwind)
+Use React to generate a web app with Tailwind CSS. It will generate code and a sandbox to preview the app within seconds of capturing your wireframe/sketch
 
-          ## Use GPT4v to generate web app code (CRA + Tailwind)
-          Use React to generate a web app with Tailwind CSS. It will generate code and a sandbox to preview the app within seconds of capturing your wireframe/sketch
-          
-          [![Sketch2Code](https://markdown-videos-api.jorgenkh.no/url?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D1VC_a0JP7TM)](https://www.youtube.com/watch?v=1VC_a0JP7TM)
-        
-          ## Running Locally
-          
-          After cloning the repo, go to [OpenAI](https://beta.openai.com/account/api-keys) to make an account and put your API key in a file called \`.env\`.
-          
-          Then, run the application in the command line and it will be available at \`http://localhost:3000\`.
-          
-          \`\`\`bash
-          npm install && npm start
-          \`\`\`
+[![Sketch2Code](https://markdown-videos-api.jorgenkh.no/url?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D1VC_a0JP7TM)](https://www.youtube.com/watch?v=1VC_a0JP7TM)
+
+Run the application in the command line and it will be available at \`http://localhost:3000\`. 
+\`\`\`bash
+npm install && npm start
+\`\`\`
         `          
         },
         "/index.js": {
           code: `import React from "react";
-      import ReactDOM from "react-dom";
-      import "./index.css";
-      import App from "./App";
-      
-      ReactDOM.render(
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>,
-        document.getElementById("root")
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
       );`,
         },
         "/index.css": {
@@ -52,17 +46,17 @@ export default function Sandbox({ mainFile, framework }) {
         },
         "/public/index.html": {
           code: `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://unpkg.com/@tailwindcss/ui/dist/tailwind-ui.min.css" rel="stylesheet">
-        <title>Document</title>
-      </head>
-      <body>
-        <div id="root"></div>
-      </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://unpkg.com/@tailwindcss/ui/dist/tailwind-ui.min.css" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body>
+  <div id="root"></div>
+</body>
       </html>`
         }
       };
@@ -130,86 +124,83 @@ export default function Sandbox({ mainFile, framework }) {
         "/App.js": mainFile,
         "/README.md": {
           code: `# [Sketch-2-App](https://www.sketch2app.io/)
+## Use GPT4v to generate web app code (CRA + Tailwind)
+Use React Native to generate a web mobile app. It will generate code and a sandbox to preview the app within seconds of capturing your wireframe/sketch
 
-          ## Use GPT4v to generate web app code (CRA + Tailwind)
-          Use React Native to generate a web mobile app. It will generate code and a sandbox to preview the app within seconds of capturing your wireframe/sketch
-          
-          [![Sketch2Code](https://markdown-videos-api.jorgenkh.no/url?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D1VC_a0JP7TM)](https://www.youtube.com/watch?v=1VC_a0JP7TM)
-        
-          ## Running Locally
-          
-          After cloning the repo, go to [OpenAI](https://beta.openai.com/account/api-keys) to make an account and put your API key in a file called \`.env\`.
-          
-          Then, run the application in the command line and it will be available at \`http://localhost:3000\`.
-          
-          \`\`\`bash
-          npm install && npm start
-          \`\`\`
+[![Sketch2Code](https://markdown-videos-api.jorgenkh.no/url?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D1VC_a0JP7TM)](https://www.youtube.com/watch?v=1VC_a0JP7TM)
+
+## Running Locally
+
+Run the application in the command line and it will be available at \`http://localhost:3000\`.
+
+\`\`\`bash
+npm install && npm start
+\`\`\`
         `          
         },
         "/package.json": {
           code: `{
-            "name": "react-native-expo",
-            "version": "1.0.0",
-            "main": "node_modules/expo/AppEntry.js",
-            "scripts": {
-              "start": "npm start",
-              "android": "expo start --android",
-              "ios": "expo start --ios",
-              "web": "expo start --web"
-            },
-            "dependencies": {
-              "@expo/cli": "^0.10.14",
-              "expo": "~49.0.16",
-              "expo-status-bar": "~1.7.1",
-              "react": "18.2.0",
-              "react-native": "0.72.6",
-              "react-native-web": "~0.19.9",
-              "react-dom": "18.2.0"
-            },
-            "devDependencies": {
-              "@babel/core": "^7.23.2",
-              "body-parser": "^1.18.3",
-              "express": "^4.16.3",
-              "http-proxy-middleware": "^2.0.6",
-              "qs": "^6.11.2",
-              "xdl": "^60.0.12"
-            },
-            "private": true
-          }
+"name": "react-native-expo",
+"version": "1.0.0",
+"main": "node_modules/expo/AppEntry.js",
+"scripts": {
+  "start": "npm start",
+  "android": "expo start --android",
+  "ios": "expo start --ios",
+  "web": "expo start --web"
+},
+"dependencies": {
+  "@expo/cli": "^0.10.14",
+  "expo": "~49.0.16",
+  "expo-status-bar": "~1.7.1",
+  "react": "18.2.0",
+  "react-native": "0.72.6",
+  "react-native-web": "~0.19.9",
+  "react-dom": "18.2.0"
+},
+"devDependencies": {
+  "@babel/core": "^7.23.2",
+  "body-parser": "^1.18.3",
+  "express": "^4.16.3",
+  "http-proxy-middleware": "^2.0.6",
+  "qs": "^6.11.2",
+  "xdl": "^60.0.12"
+},
+"private": true
+}
           `,
         },
         "/app.json": {
           code:`
-        {
-          "expo": {
-            "name": "Sketch2App Demo App",
-            "slug": "react-native-sketch2app",
-            "version": "1.0.0",
-            "orientation": "portrait",
-            "icon": "./assets/icon.png",
-            "userInterfaceStyle": "light",
-            "splash": {
-              "image": "./assets/splash.png",
-              "resizeMode": "contain",
-              "backgroundColor": "#ffffff"
-            },
-            "assetBundlePatterns": ["**/*"],
-            "ios": {
-              "supportsTablet": true
-            },
-            "android": {
-              "adaptiveIcon": {
-                "foregroundImage": "./assets/adaptive-icon.png",
-                "backgroundColor": "#ffffff"
-              }
-            },
-            "web": {
-              "bundler": "metro",
-              "favicon": "./assets/favicon.png"
-            }
-          }
-        }        
+{
+  "expo": {
+    "name": "Sketch2App Demo App",
+    "slug": "react-native-sketch2app",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "userInterfaceStyle": "light",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "assetBundlePatterns": ["**/*"],
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      }
+    },
+    "web": {
+      "bundler": "metro",
+      "favicon": "./assets/favicon.png"
+    }
+  }
+}        
         `},
         "/babel.config.js": {code: 
         `module.exports = function(api) {

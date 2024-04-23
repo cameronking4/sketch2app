6fce697957eb800f8aa3d86270e8fc30e258abbc
Cameron King <35708477+cameronking4@users.noreply.github.com>
2024-04-23 08:18:48 +0000

    sketch2app-1.0-deprecation

--- app/(origin)/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Spinner = () => {
-  return (
-    <div class="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
-  );
-}
-
-export default Spinner;
